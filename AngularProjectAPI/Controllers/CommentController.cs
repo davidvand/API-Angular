2d4adb7f4e696c6e3c4c52a560df77567f8cdb92
davidvand <r0761104@student.thomasmore.be>
2020-11-27 18:58:10 +0100

    first commit

---
+++ AngularProjectAPI/Controllers/CommentController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AngularProjectAPI.Data;
+using AngularProjectAPI.Models;
+
+namespace AngularProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+                private readonly NewsContext _context;
+
+        public CommentController(NewsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Comment
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comment>>> GetComments()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        // GET: api/Comment/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comment>> GetComment(int id)
+        {
+            var Comment = await _context.Comments.FindAsync(id);
+
+            if (Comment == null)
+            {
+                return NotFound();
+            }
+
+            return Comment;
+        }
+
+        // PUT: api/Comment/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComment(int id, Comment Comment)
+        {
+            if (id != Comment.CommentID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(Comment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Comment
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Comment>> PostComment(Comment Comment)
+        {
+            _context.Comments.Add(Comment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetComment", new { id = Comment.CommentID }, Comment);
+        }
+
+        // DELETE: api/Comment/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Comment>> DeleteComment(int id)
+        {
+            var Comment = await _context.Comments.FindAsync(id);
+            if (Comment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comments.Remove(Comment);
+            await _context.SaveChangesAsync();
+
+            return Comment;
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comments.Any(e => e.CommentID == id);
+        }
+    
+
+}
+}

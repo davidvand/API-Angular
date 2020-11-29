c776a63c2734a33020f2f73074680204ad831146
davidvand <r0761104@student.thomasmore.be>
2020-11-29 18:37:00 +0100

    comment change

--- AngularProjectAPI/Models/Comment.cs
+++ AngularProjectAPI/Models/Comment.cs
@@ -9,8 +9,8 @@ namespace AngularProjectAPI.Models
     {
         public int CommentID { get; set; }
         public string Text { get; set; }
-        public int UserID { get; set; }
-        public User User { get; set; }
+        public int? UserID { get; set; }
+        public User? User { get; set; }
         public int ArticleID { get; set; }
 
         public Article Article { get; set; }
